13f9fd51972b2ff36b9dd0226608189521679f58
Justin L <jel2658@gmail.com>
2021-11-30 14:27:36 -0500

    Worked on enemy walking script and animations

--- Sawed-off/Assets/Scripts/EnemyWalk.cs
+++ Sawed-off/Assets/Scripts/EnemyWalk.cs
@@ -6,16 +6,28 @@ public class EnemyWalk : MonoBehaviour
 {
 
     Animator anim;
+    public GameObject player;
+    public float speed;
+    private Transform target;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        target = player.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (player.transform.position.x < transform.position.x)
+        {
+            anim.SetBool("WalkingLeft", true);
+        } else if (player.transform.position.x > transform.position.x)
+        {
+            anim.SetBool("WalkingLeft", false);
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
     }
 }
