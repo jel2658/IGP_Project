1ca963ad488ec2e25514f6407b13f028bd02efc0
Justin L <jel2658@gmail.com>
2021-12-03 15:15:31 -0500

    enemy attack and player health scripts created and started

---
+++ Sawed-off/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int startingHealth;
+    public int currentHealth;
+    bool isDead;
+
+    void Awake()
+    {
+        currentHealth = startingHealth;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isDead = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        GetComponent<player>().enabled = false;
+        transform.Rotate(0f, 45f, 0f, Space.Self);
+    }
+}
