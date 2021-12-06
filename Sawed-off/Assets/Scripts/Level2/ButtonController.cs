512020642c1e8091e33a6d9a02724fa2eb947aec
Justin L <jel2658@gmail.com>
2021-12-05 19:29:15 -0500

    Level 2 button, enemy range

---
+++ Sawed-off/Assets/Scripts/Level2/ButtonController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    public GameObject button;
+    public GameObject buttonPressed;
+
+    public bool pressed;
+
+    void Awake()
+    {
+        //button = Transform.Find("button");
+        //buttonPressed = Transform.Find("button_pressed");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pressed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            buttonPressed.SetActive(true);
+            button.SetActive(false);
+            pressed = true;
+        }
+    }
+}
---
+++ Sawed-off/Assets/Scripts/Level2/EnemyRange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRange : MonoBehaviour
+{
+    public GameObject boundary;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.position.x > boundary.transform.position.x && player.transform.position.x < transform.position.x)
+        {
+            GetComponent<EnemyWalk>().enabled = true;
+            enabled = false;
+        }
+    }
+}
