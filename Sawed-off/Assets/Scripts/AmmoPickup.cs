90cba3d8155188ed580b18d7c8a6a1757a4ecd13
Justin L <jel2658@gmail.com>
2021-12-05 21:53:02 -0500

    ammo pickup script, level 2 updates

--- Sawed-off/Assets/Scripts/AmmoPickup.cs
+++ Sawed-off/Assets/Scripts/AmmoPickup.cs
@@ -4,23 +4,42 @@ using UnityEngine;
 
 public class AmmoPickup : MonoBehaviour
 {
+    public int ammo;
+    public float pickupRespawn;
+
+    bool pickedUp;
+    float timer;
+    SpriteRenderer spriteRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        pickedUp = false;
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (pickedUp)
+        {
+            spriteRenderer.enabled = false;
+            timer += Time.deltaTime;
+            if (timer >= pickupRespawn)
+            {
+                pickedUp = false;
+                spriteRenderer.enabled = true;
+                timer = 0f;
+            }
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" && !pickedUp)
         {
-
+            other.GetComponent<player>().Reload(ammo);
+            pickedUp = true;
         }
     }
 }
