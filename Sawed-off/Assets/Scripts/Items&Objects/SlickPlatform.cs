f20120b94bb0421971f1f5a8580a3b5b24b5c333
Jkirby243 <60945819+Jkirby243@users.noreply.github.com>
2021-12-10 03:07:35 -0500

    Added SlickPlatform for Level 3 & updated player LayerMask cause past me was very very dumb

---
+++ Sawed-off/Assets/Scripts/Items&Objects/SlickPlatform.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlickPlatform : MonoBehaviour
+{
+    float drag = 0.2f;
+    public float newdrag;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Player") == true)
+        {
+            collision.GetComponent<Rigidbody2D>().drag = newdrag;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.transform.CompareTag("Player") == true)
+        {
+            other.GetComponent<Rigidbody2D>().drag = drag;
+        }
+    }
+}
