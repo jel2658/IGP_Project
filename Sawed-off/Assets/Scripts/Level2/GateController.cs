770d1c765606abbe182fea0b94c3407523699af8
Jkirby243 <60945819+Jkirby243@users.noreply.github.com>
2021-12-10 00:56:02 -0500

    Git hub problems
    This reverts commit 4132713d4f48489cc5b31246f570e480ee030656.

---
+++ Sawed-off/Assets/Scripts/Level2/GateController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateController : MonoBehaviour
+{
+    public GameObject button;
+
+    bool unlocked;
+    Transform gateClosed;
+    Transform gateOpen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //unlocked = button.GetComponent<ButtonController>().pressed;
+        gateClosed = transform.Find("gateClosed");
+        gateOpen = transform.Find("gateOpen");
+        gateOpen.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (button.GetComponent<ButtonController>().pressed)
+        {
+            gateClosed.gameObject.SetActive(false);
+            gateOpen.gameObject.SetActive(true);
+            enabled = false;
+        }
+    }
+}
