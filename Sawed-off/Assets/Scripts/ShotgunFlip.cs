d86d7eefa99d40cf7b25fa7f9575c241600bf9ea
Jkirby243 <60945819+Jkirby243@users.noreply.github.com>
2021-12-11 23:12:05 -0500

    Added Sawed off sprite and sprite flip code

---
+++ Sawed-off/Assets/Scripts/ShotgunFlip.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotgunFlip : MonoBehaviour
+{
+    public GameObject Right;
+    public GameObject Left;
+    public Transform pivot;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pivot.eulerAngles.z >= 180.01f && pivot.eulerAngles.z <= 359.9f)
+        {
+            //Debug.Log("Facing right");
+            //The left shotgun turns off and right turns on
+            Right.SetActive(true);
+            Left.SetActive(false);
+            //This solution is awful but it works
+        }
+        if (pivot.eulerAngles.z >= .01f && pivot.eulerAngles.z <= 179.9)
+        {
+            //Debug.Log("facing left");
+            //The left shotgun turns off and right turns on
+            Right.SetActive(false);
+            Left.SetActive(true);
+            //This solution is awful but it works
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Debug.Log(pivot.eulerAngles.z);
+        }
+    }
+}
